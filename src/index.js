1efda0b12f2c5e9d5f6d12b15104dad44df188dd
danieltadresu <danieldev1999@gmail.com>
2021-02-14 17:17:02 -0300

    improvement: Combining Multiple Reducers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'; // WE NEED TO HAVE REDUX INSTALLED IN OUR package.-lock.json
+import { createStore, combineReducers } from 'redux'; // WE NEED TO HAVE REDUX INSTALLED IN OUR package.-lock.json
 import { Provider } from 'react-redux'; // WE NEED TO HAVE react-redux TO CONNECT THE REACT APP WITH REDUX 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import reducer from './store/reducer';
+// import reducer from './store/reducer';
+import counterReducer from './store/reducers/counter';
+import resultReducer from './store/reducers/result';
 
+const rootReducer = combineReducers({
+    ctr: counterReducer,
+    res: resultReducer
+});
 
 // CREATE STORE WITH createStore() METHOD FROM REDUX
-const store = createStore(reducer) 
+// const store = createStore(reducer) 
+const store = createStore(rootReducer) 
 
 ReactDOM.render(
     <Provider store={store}> {/* WITH THAT, OUR STORE IS CONNECTED WITH OUR REACT APP*/}
