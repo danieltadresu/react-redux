9eae3bbe499a7ba71bd736d67383771c1e9aaa36
danieltadresu <danieldev1999@gmail.com>
2021-02-16 23:00:03 -0300

    improvement: Adding Middleware

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux'; // WE NEED TO HAVE REDUX INSTALLED IN OUR package.-lock.json
 import { Provider } from 'react-redux'; // WE NEED TO HAVE react-redux TO CONNECT THE REACT APP WITH REDUX 
+import { createStore, combineReducers, applyMiddleware } from 'redux'; // WE NEED TO HAVE REDUX INSTALLED IN OUR package.-lock.json
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -15,9 +15,20 @@ const rootReducer = combineReducers({
     res: resultReducer
 });
 
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware next state', store.getState())
+            return result;
+        }
+    }
+}
+
 // CREATE STORE WITH createStore() METHOD FROM REDUX
 // const store = createStore(reducer) 
-const store = createStore(rootReducer) 
+const store = createStore(rootReducer, applyMiddleware(logger)) 
 
 ReactDOM.render(
     <Provider store={store}> {/* WITH THAT, OUR STORE IS CONNECTED WITH OUR REACT APP*/}
